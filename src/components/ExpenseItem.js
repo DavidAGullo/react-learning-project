34f5b4016766621cce1000d9985da6791dc54bd4
DavidAGullo <60278018+DavidAGullo@users.noreply.github.com>
2022-10-02 13:59:35 -0500

    initial

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,23 @@
+//Functions can only have one return statement with one root element
+import './ExpenseItem.css'; //importing css file
+
+const expenseDate = new Date(2021, 2, 28); //month is 0 based
+const expenseTitle = 'Car Insurance';
+const expenseAmount = precise(250.64);
+
+function ExpenseItem(props) { //props is an object, props are the most important thing in React
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${precise(props.amount)}</div>
+      </div>
+    </div>
+  );
+}
+
+function precise(x) {
+  return x.toFixed(2);
+}
+export default ExpenseItem;
