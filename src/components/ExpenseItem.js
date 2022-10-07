71d687d0fadf90d20319a95c2faafd9436a59a56
DavidAGullo <60278018+DavidAGullo@users.noreply.github.com>
2022-10-07 10:52:43 -0500

    ExpenseDate.js New Component
    Removed Work from ExpenseItem to calculate date information and made it a child to process when we need it, Named ExpenseDate.js
    
    Removed Date from ExpenseItem
    Added new Comments
    Added new Component (ExpenseDate.js)
    Updated format of Date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,17 @@
 //Functions can only have one return statement with one root element
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'; //importing css file
 
 //We don't need these anymore because we are using the ExpenseItem props
+/*
 const expenseDate = new Date(2021, 2, 28); //month is 0 based
 const expenseTitle = 'Car Insurance';
 const expenseAmount = precise(250.64);
-
+*/
 function ExpenseItem(props) { //props is an object, props are the most important thing in React
-  const month = props.date.toLocaleString('en-US', {month: 'long'});
-  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} /> 
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${precise(props.amount)}</div>
@@ -24,6 +19,9 @@ function ExpenseItem(props) { //props is an object, props are the most important
     </div>
   );
 }
+//ExpenseData in ExpenseItem.js has self closing tag because it is a component without any content
+//Props are used here to cast data from the parent component to the child component
+//props from App.js are passed to ExpenseItem.js which then passes them to ExpenseDate.js
 
 function precise(x) {
   return x.toFixed(2); //returns a number with 2 decimal places (e.g. 250.64)
