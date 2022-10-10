8917fa2b3c797d690d58085ce5f897f084eb4117
DavidAGullo <60278018+DavidAGullo@users.noreply.github.com>
2022-10-09 21:30:42 -0500

    Update ExpenseItem.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,5 @@
 //Functions can only have one return statement with one root element
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css'; //importing css file
@@ -9,15 +10,19 @@ const expenseDate = new Date(2021, 2, 28); //month is 0 based
 const expenseTitle = 'Car Insurance';
 const expenseAmount = precise(250.64);
 */
-function ExpenseItem(props) { //props is an object, props are the most important thing in React
+const ExpenseItem = (props) => { //props is an object, props are the most important thing in React
+  const [title, setTitle] = useState(props.title); //this is a hook, it allows us to use state in functional components
+
   const clickHandler = () => {
-    console.log('Clicked!');
-  }; //this is a function that is called when the button is clicked
+    setTitle('Updated!');
+    console.log(title);
+  } //this is a function that is called when the button is clicked
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} /> 
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${precise(props.amount)}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
