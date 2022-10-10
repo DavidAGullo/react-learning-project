e9e53cd7c75bd8829f99f96221075f2b9cff0c11
DavidAGullo <60278018+DavidAGullo@users.noreply.github.com>
2022-10-10 11:00:19 -0500

    Update ExpenseForm.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,12 +2,22 @@ import React from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+    // onChange is an event listener that listens for changes in the input field
+    // onChange is a built in event listener
+    // onChange is a function that is called when the input field is changed
+
+    // onChange is different from onClick because onClick is called when the button is clicked
+    const titleChangeHandler = (event) => {
+        console.log("Title Changed!");
+        console.log(event.target.value); //event.target.value is the value of the input field
+    };
+
     return (
         <form>
         <div className="new-expense__controls">
             <div className="new-expense__control">
             <label>Title</label>
-            <input type="text" />
+            <input type="text" onChange={titleChangeHandler}/>
             </div>
             <div className="new-expense__control">
             <label>Amount</label>
