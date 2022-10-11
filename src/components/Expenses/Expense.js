4e1e0475f618d51fcbe8261f2cd4ac0bebf17d1f
DavidAGullo <60278018+DavidAGullo@users.noreply.github.com>
2022-10-11 17:26:45 -0500

    Added Default State to last commit

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -4,6 +4,8 @@ import './Expense.css';
 import ExpensesFilter from "./ExpenseFilter";
 
 function Expense(props) {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
     const filterChangeHandler = (selectedYear) => {
         console.log(selectedYear);
     }
@@ -11,7 +13,7 @@ function Expense(props) {
     return (
         <div>     
             <Card className='expenses'>
-                <ExpensesFilter onFilterChange={filterChangeHandler}/>
+                <ExpensesFilter selected={filterYear} onFilterChange={filterChangeHandler}/>
                 <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}></ExpenseItem> 
                 <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}></ExpenseItem>
                 <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date}></ExpenseItem>
